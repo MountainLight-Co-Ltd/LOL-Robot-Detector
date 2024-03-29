02322b02652c817e3e2b8cc2e7c1f6055fc33cb5
DongNanQing <165023476+Solistoria@users.noreply.github.com>
2024-03-30 00:14:25 +0800

    Delete yolov8Trainer.py

--- yolov8Trainer.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('yolov8x.pt')
-# model = YOLO('yolov8n.pt')
-
-results = model.train(data='dataset.yaml', epochs=100, workers=0, patience=5, batch=128, imgsz=640)
